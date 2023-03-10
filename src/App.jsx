6b2dcc5796f29cc68632c02b12f0c3c32df295f9
fn-Drew <alutter03@gmail.com>
2023-03-09 20:11:43 -0500

    html element below 3d text

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+/* eslint-disable react/no-unknown-property */
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Text3D, Float } from '@react-three/drei';
+import {
+    OrbitControls, Text3D, Float, Center, Html,
+} from '@react-three/drei';
 import { NodeToyMaterial, NodeToyTick } from '@nodetoy/react-nodetoy';
 import glitchShader from './glitchData';
 import './App.css';
@@ -10,31 +13,47 @@ function Text() {
     const name = 'Andrew\nGarfunkel';
     return (
         <Float>
-            <Text3D
-                font={font}
-                size={0.25}
-                bevelEnabled
-                bevelSize={0.02}
-                height={0.1}
-                bevelSegments={20}
-            >
-                {name}
-                <NodeToyMaterial data={glitchShader} />
-            </Text3D>
+            <mesh rotation={[0.18, 0, 0]}>
+                <Text3D
+                    font={font}
+                    size={0.175}
+                    bevelEnabled
+                    bevelSize={0.01}
+                    height={0.05}
+                    bevelSegments={10}
+                    position={[-0.375, 1, 0]}
+                >
+                    {name}
+                    <NodeToyMaterial data={glitchShader} />
+                </Text3D>
+            </mesh>
         </Float>
     );
 }
 
 export default function App() {
     return (
-        <Canvas camera={{
-            fov: 75, near: 0.1, far: 1000, position: [0, 0, 2],
-        }}
+        <Canvas
+            camera={{
+                fov: 75, near: 0.1, far: 1000, position: [0, 0, 2],
+            }}
         >
-            <Text />
-            <directionalLight position={[0, 0, 5]} intensity={0.5} />
             <NodeToyTick />
-            <OrbitControls />
+            <OrbitControls enableZoom={false} />
+            <directionalLight position={[0, 0, 5]} intensity={0.5} />
+
+            <Center disableY>
+                <Html>
+                    <div style={{ color: 'red', fontSize: 40 }}>
+                        ega goga
+                    </div>
+                    <div style={{ color: 'red' }}>
+                        ega goga
+                    </div>
+                </Html>
+                <Text />
+            </Center>
+
         </Canvas>
     );
 }
