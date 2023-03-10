a6150ea400695f0f3da9f5d9cc9a009f3224944e
fn-Drew <alutter03@gmail.com>
2023-03-10 16:05:44 -0500

    component showing relevant tech skills

--- src/App.jsx
+++ src/App.jsx
@@ -50,16 +50,28 @@ export default function App() {
                 </Canvas>
             </div>
 
-            <div className="basis-1/2 snap-x flex overflow-x-scroll ">
+            <div className="basis-1/2 snap-x flex overflow-x-scroll">
+
+                {/* project component */}
+                <div className="snap-center bg-slate-800/70 flex rounded-3xl flex-col place-content-center w-3/4 m-32 text-center text-white flex-shrink-0">
+                    <h1 className="text-2xl font-bold p-2">NFT Validator</h1>
+                    <p className="text-lg text-gray-300 p-2">tap to view more</p>
+                </div>
+
+                {/* tech component */}
+                <div className="snap-center grid grid-cols-2 bg-slate-800/70 rounded-3xl text-center w-3/4 m-16 text-white flex-shrink-0 ">
+                    <div className="tech-icon hover:bg-blue-500 rounded-tl-3xl bg-blue-500/25 "> react </div>
+                    <div className="tech-icon hover:bg-cyan-500 rounded-tr-3xl bg-cyan-500/25"> tailwind </div>
+                    <div className="tech-icon hover:bg-[rgb(0,122,204)] bg-[rgb(0,122,204)]/25"> typescript </div>
+                    <div className="tech-icon hover:bg-green-500 bg-green-500/25"> node </div>
+                    <div className="tech-icon hover:bg-yellow-500 rounded-bl-3xl bg-yellow-500/25"> express </div>
+                    <div className="tech-icon hover:bg-green-500 rounded-br-3xl bg-green-500/25"> mongo </div>
+                </div>
+
                 <div className="snap-center m-32 text-center text-white flex-shrink-0 w-64 bg-slate-700">
                     aroistenrasoiet
                 </div>
-                <div className="snap-center m-32 text-center text-white flex-shrink-0 w-64 bg-slate-700">
-                    aroistenrasoiet
-                </div>
-                <div className="snap-center m-32 text-center text-white flex-shrink-0 w-64 bg-slate-700">
-                    aroistenrasoiet
-                </div>
+
             </div>
 
         </div>
@@ -67,4 +79,4 @@ export default function App() {
 }
 
 // TODO
-// - adjust glitchShader shader for size of text
+// - adjust glitch shader for size of text
