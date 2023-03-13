ca162c07e216c10b568215614b18350f757cf172
fn-Drew <alutter03@gmail.com>
2023-03-12 20:32:18 -0400

    current component display at bottom of the screen

--- src/App.jsx
+++ src/App.jsx
@@ -171,6 +171,12 @@ export default function App() {
 
             </div>
 
+            <div className="flex justify-center space-x-8 py-8">
+                <input disabled defaultChecked={visibleContent.bio} type="radio" className="radio border-none radio-xs disabled:opacity-100" />
+                <input disabled defaultChecked={visibleContent.tech} type="radio" className="radio border-none radio-xs disabled:opacity-100" />
+                <input disabled defaultChecked={visibleContent.nft} type="radio" className="radio border-none radio-xs disabled:opacity-100" />
+            </div>
+
         </div>
     );
 }
